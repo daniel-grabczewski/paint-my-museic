f4b87a45cab7d5cf0123462293478544e541c813
Daniel <danielgrabczewski@gmail.com>
2023-06-02 14:26:26 +1200

    add images

--- client/data/data.ts
+++ client/data/data.ts
@@ -4,30 +4,35 @@ const data = [
     code: '#A71814',
     music: 'client/public/music/1_Red_Spiderman.mp3',
     isPicked: false,
+    image: 'client/public/images/spiderman.jpg',
   },
   {
     color: 'blue',
     code: '#383686',
     music: 'client/public/music/2_Blue_Superman.mp3',
     isPicked: false,
+    image: 'client/public/images/starwars.jpg',
   },
   {
     color: 'green',
     code: '#88C752',
     music: 'client/public/music/3_Green_Lord of the Rings.mp3',
     isPicked: false,
+    image: 'client/public/images/lordoftherings.jpg',
   },
   {
     color: 'black',
     code: '#181818',
     music: 'client/public/music/4_Black_Star Wars.mp3',
     isPicked: false,
+    image: '',
   },
   {
     color: 'pink',
     code: '#E664bf',
     music: 'client/public/music/5_Pink_Barbie.mp3',
     isPicked: false,
+    image: 'client/public/images/barbie.jpg',
   },
   // {
   //   color: 'random',
@@ -40,6 +45,7 @@ const data = [
     code: 'white',
     music: 'client/public/music/7_White_Mad World.mp3',
     isPicked: false,
+    image: 'client/public/images/eraser.jpg',
   },
   // {
   //   color: 'none',
